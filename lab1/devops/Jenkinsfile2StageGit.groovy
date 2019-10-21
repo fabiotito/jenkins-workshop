d5799e6241b26df2c30d9da67cb63e7c64ed8d00
Ubuntu <devopsuser@devops.bhoivqdzcy0epmswu1ycuzbp3d.bx.internal.cloudapp.net>
2019-10-21 23:28:30 +0000

    primer job

---
+++ lab1/devops/Jenkinsfile2StageGit.groovy
@@ -0,0 +1,14 @@
+
+node{
+    withEnv(['ENV_CREDENTIALS=global-github-opando']) {
+        stage("init"){
+            
+            echo "TODO"
+            
+
+        }
+        stage("checkout"){
+            
+        }
+    }
+}
