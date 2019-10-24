0aa53fbb5eeff1ec13b7fb697459226fe682cf49
fabiotito <fabiotito1@outlook.com>
2019-10-24 18:59:43 -0500

    aks pipeline

--- lab1/devops/Jenkinsfile3Credentials.groovy
+++ lab1/devops/Jenkinsfile3Credentials.groovy
@@ -4,14 +4,14 @@ node{
 
     }
     stage("checkout"){
-        withCredentials([string(id:"ftito-token",variable:'token1')]){
+        withCredentials([string(credentialsId:"ftito-token",variable:'token1')]){
             echo "${token1}"
 
             tokentmp = token1.split('\\|')
             tok1 = tokentmp[0]
             tok1 = tokentmp[1]
 
-            echo "${tok1} ${tok2}"
+            echo "${tok1} ---- ${tok2}"
         }
 
     }
