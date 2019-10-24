e4887c35fbf5168858d68613b2b6250da54bed86
fabiotito <fabiotito1@outlook.com>
2019-10-24 18:58:57 -0500

    aks pipeline

--- lab1/devops/Jenkinsfile3Credentials.groovy
+++ lab1/devops/Jenkinsfile3Credentials.groovy
@@ -1,4 +1,23 @@
 
 node{
+    stage("init"){
+
+    }
+    stage("checkout"){
+        withCredentials([string(id:"ftito-token",variable:'token1')]){
+            echo "${token1}"
+
+            tokentmp = token1.split('\\|')
+            tok1 = tokentmp[0]
+            tok1 = tokentmp[1]
+
+            echo "${tok1} ${tok2}"
+        }
+
+    }
     
 }
+
+/*
+Tecsup00|fabiotito@gmail.com|textosecreto
+*/
