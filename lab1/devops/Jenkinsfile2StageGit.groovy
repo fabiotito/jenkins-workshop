f4c6e3197e7cfdfafa3bb8ccaa1e3bb2432e107a
fabiotito <fabiotito1@outlook.com>
2019-10-24 18:48:06 -0500

    aks pipeline

--- lab1/devops/Jenkinsfile2StageGit.groovy
+++ lab1/devops/Jenkinsfile2StageGit.groovy
@@ -3,22 +3,27 @@ node{
     withEnv(["ENV_CLIENTID=1234-123"]){
 
         stage ("init"){
-            echo "${ENV_CLIENTID}"
-sh 'printenv'
+        echo "${ENV_CLIENTID}"
+        sh 'printenv'
+        cleanWs()
         }
   
     }
 
     stage("checkout"){
         println '====TODO===='
-
+        
+        sh "mkdir source"
+        dir("source"){
         git(
             branch : "master",
             credentialsId: "ftito-github",
             url: "https://github.com/fabiotito/aks-rbac-example1.git"
         )
-
-        sh "ls -lta"
+        }
+        sh "pwd"
+        
+        sh "ls -lta && cd source && ls -lta"
 
     }
    
