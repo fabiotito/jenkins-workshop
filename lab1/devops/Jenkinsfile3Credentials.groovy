b9b8ef56a31d4d8037ad88177ffa9047976c46f0
fabiotito <fabiotito1@outlook.com>
2019-10-24 19:16:11 -0500

    aks pipeline

--- lab1/devops/Jenkinsfile3Credentials.groovy
+++ lab1/devops/Jenkinsfile3Credentials.groovy
@@ -5,11 +5,12 @@ node{
     }
     stage("checkout"){
         withCredentials([string(credentialsId:"ftito-token",variable:'token1')]){
+            
             echo "${token1}"
 
             tokentmp = token1.split('\\|')
             tok1 = tokentmp[0]
-            tok1 = tokentmp[1]
+            tok2 = tokentmp[1]
 
             echo "${tok1} ---- ${tok2}"
         }
--- lab1/devops/Jenkinsfile4Docker.groovy
+++ lab1/devops/Jenkinsfile4Docker.groovy
@@ -1,4 +1,32 @@
 
 node{
-    
+    //docker pull hashicorp/terraform:0.12.12
+
+    stage("init"){
+
+    }
+    stage("checkout"){
+        withCredentials([string(credentialsId:"ftito-token",variable:'token1')]){
+            
+            echo "${token1}"
+
+            tokentmp = token1.split('\\|')
+            tok1 = tokentmp[0]
+            tok2 = tokentmp[1]
+
+            echo "${tok1} ---- ${tok2}"
+        }
+
+    }
+
+// terraform init
+// terraform apply
+// terraform -version
+// docker run -it <name image>:tag
+// docker pull docker pull hashicorp/terraform:0.12.12
+    stage("deploy"){
+        docker.image("docker pull hashicorp/terraform:0.12.12").withRun("-version"){
+            container -> sh "docker logs ${container.id}"
+        }
+    }
 }
